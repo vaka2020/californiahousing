ab51211d411cf8823ef492c04968ed6afc8052f5
Chiru Anand <vchiruanand@gmail.com>
2025-12-18 00:05:23 -0500

    deleted procfile

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def predict():
     return render_template("home.html", prediction_text="The predicted house price is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)    
+    app.run(debug=False)    
